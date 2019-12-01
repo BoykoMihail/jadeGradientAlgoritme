c4c9c5dc0af1ab152b3281184ca5420ba6935dec
BoykoMihail <boyko.mihail.al@gmail.com>
2019-12-01 14:50:16 +0300

    version 2.0 Add delay

--- src/jada_second_algoritm_boyko/OrderAgent.java
+++ src/jada_second_algoritm_boyko/OrderAgent.java
@@ -11,6 +11,8 @@ import jade.core.behaviours.SimpleBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,31 +21,23 @@ import java.util.Random;
 public class OrderAgent extends Agent {
 
     private double number;
-    private int countOfNeighbors;
     private String[] neighborsIdentifire;
     private Random random = new Random();
-    private double summ = 0;
-    private double ratio = 1;
+    private boolean isSend = false;
+    private boolean isUpdate = false;
+    private double probabilityOfFailure = 1;
+    private double probabilityOfDelay = 1;
 
     protected void setup() {
 
         Object[] args = getArguments();
         if (args != null && args.length > 1) {
-            ratio = Double.parseDouble((String) args[0]);
-            
-            number = Integer.parseInt((String) args[1]);
-            System.out.println("Number is " + number);
-
-            countOfNeighbors = Integer.parseInt((String) args[2]);
-            System.out.println("countOfNeighbors is " + countOfNeighbors);
-
-            if (args.length < 3 + countOfNeighbors) {
-                System.out.println("No neighborsdentifire");
-                doDelete();
-            }
-            neighborsIdentifire = new String[countOfNeighbors];
-            for (int i = 0; i < countOfNeighbors; ++i) {
-                neighborsIdentifire[i] = (String) args[i + 3];
+            probabilityOfFailure = Double.parseDouble((String) args[0]);
+            probabilityOfDelay = Double.parseDouble((String) args[1]);
+            number = Integer.parseInt((String) args[2]);
+            neighborsIdentifire = new String[args.length - 3];
+            for (int i = 3; i < args.length; ++i) {
+                neighborsIdentifire[i - 3] = (String) args[i];
             }
         } else {
             System.out.println("No number");
@@ -53,16 +47,23 @@ public class OrderAgent extends Agent {
         addBehaviour(new SimpleBehaviour(this) {
 
             public void action() {
-                ACLMessage msgNew = new ACLMessage(ACLMessage.INFORM);
-                msgNew.setOntology("MessAboutMean_Number");
-                msgNew.setContent((number + random.nextGaussian() * 0.1) + "");
-                for (int i = 0; i < neighborsIdentifire.length; i++) {
-                    msgNew.addReceiver(new AID(neighborsIdentifire[i] + "@10.42.0.1:1099/JADE"));
-//                    System.out.println("MessAboutMean_Gradient send: " + msgNew.getSender().getLocalName() + "->" + getLocalName() + " summ = " + number);
+                if (getLocalName().compareTo("8") == 0 && !isSend || isUpdate) {
+                    ACLMessage startMsg = new ACLMessage(ACLMessage.INFORM);
+                    startMsg.setOntology("Send_N");
+                    startMsg.setContent((number + random.nextGaussian() * 0.01) + "");
+                    for (int i = 0; i < neighborsIdentifire.length; i++) {
+                        startMsg.addReceiver(new AID(neighborsIdentifire[i] + "@10.42.0.1:1099/JADE"));
+                    }
+                    send(startMsg);
+                    isSend = true;
+                    isUpdate = false;
                 }
-                send(msgNew);
 
-                sendSelfGrad();
+                try {
+                    sendSelfGrad();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(OrderAgent.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
 
             @Override
@@ -70,33 +71,33 @@ public class OrderAgent extends Agent {
                 return false;
             }
 
-            private void sendSelfGrad() {
+            private void sendSelfGrad() throws InterruptedException {
 
                 MessageTemplate m1 = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
-                MessageTemplate m2 = MessageTemplate.MatchOntology("MessAboutMean_Number");
+                MessageTemplate m2 = MessageTemplate.MatchOntology("Send_N");
                 MessageTemplate m3 = MessageTemplate.and(m1, m2);
                 ACLMessage msg = blockingReceive(m3, 1200);
                 if (msg != null) {
-                    String num = msg.getContent();
-                    System.out.println("MessAboutMean_Number give: " + msg.getSender().getLocalName() + "->" + getLocalName() + " summ = " + num);
-                    summ += (Double.parseDouble(num) - number);
-                    
-                    if (Math.log(Math.random()) > Math.log(1.0 - ratio)) {
-                        System.out.println("1 of : " + getLocalName() + " number = " + number);
-                        number = number + (0.1/countOfNeighbors) * summ ;
-                        summ = 0;
-
-                        ACLMessage msgNew = new ACLMessage(ACLMessage.INFORM);
-                        msgNew.setOntology("MessAboutMean_Number");
-                        msgNew.setContent((number + random.nextGaussian() * 0.1) + "");
-//                        msgNew.setContent((number) + "");
-                        for (int i = 0; i < neighborsIdentifire.length; i++) {
-                            msgNew.addReceiver(new AID(neighborsIdentifire[i] + "@10.42.0.1:1099/JADE"));
-//                            System.out.println("MessAboutMean_Gradient send: " + msgNew.getSender().getLocalName() + "->" + getLocalName() + " summ = " + number);
+                    String numberOfMes = msg.getContent();
+                    System.out.println("Send_N from " + msg.getSender().getLocalName() + " to " + getLocalName() + " = " + numberOfMes);
+                    double grad = (Double.parseDouble(numberOfMes) - number);
+                    number = number + (0.01) * grad;
+                    isUpdate = true;
+                    if (Math.log(Math.random()) > Math.log(1.0 - probabilityOfFailure)) {
+                        if (Math.log(Math.random()) > Math.log(1.0 - probabilityOfDelay)) {
+                            Random randomNum = new Random();
+                            Thread.sleep(0 + randomNum.nextInt(2000));
                         }
-                        send(msgNew);
-                   } else {
-                        System.out.println("0 of : " + getLocalName() + " number = " + number );
+                        ACLMessage msgUpdate = new ACLMessage(ACLMessage.INFORM);
+                        msgUpdate.setOntology("Send_N");
+                        msgUpdate.setContent((number + random.nextGaussian() * 0.01) + "");
+                        for (int i = 0; i < neighborsIdentifire.length; i++) {
+                            msgUpdate.addReceiver(new AID(neighborsIdentifire[i] + "@10.42.0.1:1099/JADE"));
+                        }
+                        send(msgUpdate);
+                        isUpdate = false;
+                    } else {
+                        System.out.println(getLocalName() + " not send!");
                     }
                 }
 
